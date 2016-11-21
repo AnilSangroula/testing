0b60d03cd3dea8831e3ef3058f282ddd06bd0de0
Sangroula <asang002@int.usc.local>
2016-11-21 14:05:24 -0600

    First commit for the Project

---
+++ test_git/src/main/java/com/test/git/TestClass.java
@@ -0,0 +1,14 @@
+package com.test.git;
+
+public class TestClass {
+
+	public int add(int a, int b){
+		int result = a + b;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
